4bcc4904386f5df2eea6be9984c32feffb63a28d
Daniel Klauser <daniel.klauser@hslu.ch>
2022-12-01 12:31:32 +0000

    updated scripts

--- inference_imx8_script.py
+++ inference_imx8_script.py
@@ -3,8 +3,9 @@ from dataset_reader import ImageAnnotationReader
 from inference.inference_tflite_imx8_ssd_model import InferenceTfliteImx8SsdNpumodel,InferenceTfliteImx8SsdCpumodel
 from inference.inference_tflite_imx8_efficient_model import InferenceTfliteImx8EfficientCpumodel,InferenceTfliteImx8EfficientNpumodel
 from inference.inference_tflite_imx8_yolov5_model import InferenceTfliteImx8Yolov5Cpumodel, InferenceTfliteImx8Yolov5Npumodel
-
 from result_generation import ResultGeneration
+from datetime import datetime
+from pathlib import Path
 
 def str2bool(v):
     if isinstance(v, bool):
@@ -22,12 +23,22 @@ parser = argparse.ArgumentParser(
                     epilog = 'Text at the bottom of help')
 
 parser.add_argument('-m','--model_path',default="/media/usb-icarus/tflite_model_eval/test/model/efficient_det_0_trial_002/efficient_det_1_320_320_002.tflite")
-parser.add_argument('-t','--test_images_path',default="/media/usb-icarus/tflite_model_eval/test/data/record")
-parser.add_argument('-d','--delegate_path',default="/usr/lib/libvx_delegate.so")
+parser.add_argument('-t','--test_images_path',default="/media/usb-icarus/test/data/record")
+parser.add_argument('-s','--save_path',default="/media/usb-icarus/test/save")
+parser.add_argument('-d','--delegate_path',default="")
 parser.add_argument('-o','--save_only', type=str2bool, nargs='?',const=True, default=True)
-parser.add_argument('-f','--filter_low_score',default=0.0)
-parser.add_argument('-i','--iou_treshold',default=0.5)
-parser.add_argument('-p','--peak_results',default="/media/usb-icarus/peak")
+parser.add_argument('-f','--filter_low_score',default=0.0,type=float)
+parser.add_argument('-i','--iou_treshold',default=0.5,type=float)
+parser.add_argument('-p','--peak_results',default="")
+parser.add_argument('-n','--output_name',default="CPU_WIN")
+
+
+def create_output_file_name(args):
+    now = datetime.now()
+    date_time = now.strftime("%m_%d_%Y_%H_%M")
+    full_output_save_path = Path(args.save_path).joinpath('{}_evaluation_filtered_score_{:.2f}_iouThrs{:.2f}_{}'.format(args.output_name,args.filter_low_score, args.iou_treshold,date_time))
+    return full_output_save_path
+
 
 if __name__ == '__main__':
     args = parser.parse_args()
@@ -55,7 +66,10 @@ if __name__ == '__main__':
     dataset_reader = ImageAnnotationReader(args.test_images_path,model.get_image_size())
     dataset_reader.load_dataset()
     
-    result_generator = ResultGeneration(dataset_reader,model,"","",args.filter_low_score,args.iou_treshold,args.save_only,args.peak_results)
+    save_file_name = create_output_file_name(args)
+    
+    result_generator = ResultGeneration(dataset_reader,model,save_file_name,"",args.filter_low_score,args.iou_treshold,args.save_only,args.peak_results)
     summarized_results, images_analyzed = result_generator.start_evaluation()
     
-    
+    # Save Inference Time
+    model.save_inference_time(save_file_name)
